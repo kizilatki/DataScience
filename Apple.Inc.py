7aa18f36560289998951d566ce8dfcc2bed44fe6
Kızıl Atkı <77049951+kizilatki@users.noreply.github.com>
2023-02-22 17:20:50 +0300

    Apple.Inc.py

--- Apple.Inc.py
+++ Apple.Inc.py
@@ -9,10 +9,24 @@ from matplotlib import pyplot as plt
 # Then, need to import the file (such as "csv" file) successfully. Be careful for typo!
 aapl = pd.read_csv("D:\PYTHON\Data Analysis/AAPL.CSV")
 appl
-# Out[1]
+  # Out[1] Please! Look at the readme.md file.
 
 
 # you can able to set index like this:
 apple= aapl.set_index("Date")
 apple.head(7)
-# Out[2] 
+  # Out[2] 
+
+# let draw only a column. Such as:
+apple_price = apple[["Adj Close"]]
+apple_price.plot(figsize=(18,9))            # set a sizes
+  # Out[3] 
+
+
+plt.plot(aapl.index,aapl["Adj Close"])
+plt.show()
+  # Out[4]
+
+
+apple.describe()     #let learn other details
+  # Out[5]
